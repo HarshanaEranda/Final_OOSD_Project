6cf7d88e1d044377e87ba445e985f1b450e4a7f2
HarshanaEranda <harshawije1998@gmail.com>
2021-11-18 15:20:25 +0530

    add new file

---
+++ Javascipt_File.js
@@ -0,0 +1,15 @@
+class CheckProfession{
+      constructor(Profession1,Profession2,Profession3,Profession4){
+            this.Profession1=Profession1;
+            this.Profession2=Profession2;
+            this.Profession3=Profession3;
+            this.Profession4=Profession4;
+      }
+      Disable_Checkbox (){
+            function ShowDetails(){
+                  
+
+
+            }
+      }
+}
