e79a7087b0f9027765870da429495d4f4ac08d03
RomanEmpire <romanreings22261@gmail.com>
2021-11-20 20:18:23 +0530

    3rd commit\rename php

---
+++ Login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+      <head>
+<link rel="stylesheet" type="text/css" href="style.css">
+      </head>
+      <body><div class="background_image"><div class="Back_img"><div><div><img src="Image/C.jpg" ></div></div></div>
+            
+            <div class="loginbox" >
+                  <img src="Image/icon.png" class="Idimage" >
+                  <br>
+                  <h1 >Login Hear</h1>
+                  <br>
+                  <form id="">
+                        <p>Username</p>
+                        <input type="text" name="username" placeholder="Enter Username">
+                        <p>Password</p>
+                        <input type="password" name="password" placeholder="Enter Password">
+                        <br><br>
+                        <input type="submit" value="Login">
+                       
+                        <br>
+                        <a href="a">Forget password</a><br>
+                        <a href="Create_Account.html">If you don't have account!!!</a>
+                  </form>
+                  
+            
+            </div>
+      </body>
+</html>
